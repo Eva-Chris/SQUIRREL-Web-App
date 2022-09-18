33dc0295f7c6332cfa343928fa77cc02ef6a6144
Eva Chrysostomaki <chrisostomaki.eva@gmail.com>
2022-09-18 20:56:19 +0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,12 +1,26 @@
 # necessary imports
 from fileinput import filename
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
+from forms import ContactForm
+from flask_mail import Message, Mail
 import pandas as pd
+import os
 
+mail = Mail()
 app = Flask(__name__, static_folder='static')
 
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
+
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = 'chrisostomaki.eva@gmail.com'
+app.config["MAIL_PASSWORD"] = os.environ['MAIL_PASSWORD']
+mail.init_app(app)
+
 # index page
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 @app.route('/index')
 def index():
     
@@ -17,25 +31,24 @@ def index():
     dfh = []
     f_score = []
 
-
-    with open('AllGroups_Goodreads_FScore.txt') as my_file:
-        count = 0;
+    with open('MovieLens_AllScores.txt') as my_file:
+        count = 1;
         for line in my_file:
-            if count<=15:
+            if count<=16:
                 ov_sat.append(line)
-            elif count>=16 and count<=31:
+            elif count>=18 and count<=33:
                 max_min.append(line)
-            elif count>=32 and count<=47:
+            elif count>=35 and count<=50:
                 ndcg.append(line)
-            elif count>=48 and count<=63:
+            elif count>=52 and count<=67:
                 dfh.append(line)
-            elif count>=64:
+            elif count>=69:
                 f_score.append(line)
             
             count+=1;
 
 
-    return render_template('index.html', value="AllGroups_Goodreads_FScore",
+    return render_template('index.html', value="MovieLens_AllScores",
                            ov_sat=ov_sat, max_min=max_min, ndcg=ndcg, dfh=dfh, f_score=f_score)
 
 # about page
@@ -43,6 +56,19 @@ def index():
 def about():
     return render_template('about.html')
 
+# contact page
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    form = ContactForm()
+    if request.method == 'POST':        
+        msg = Message(request.form.get('subject'), sender = request.form.get('email'),
+            recipients = ['chrisostomaki.eva@gmail.com'],
+            body= request.form.get('message')
+            )  
+        mail.send(msg)
+        return 'Form sent.'   
+    elif request.method == 'GET':
+        return render_template('contact.html', form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
