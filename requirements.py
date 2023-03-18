ffca537569db2440ec8a6dab7f5ffb27c22aa529
Eva Chrysostomaki <chrisostomaki.eva@gmail.com>
2023-03-18 17:56:36 +0200

    Update and rename REQUIREMENTS.txt to requirements.py

---
+++ requirements.py
@@ -0,0 +1,13 @@
+import subprocess
+
+subprocess.run(["pip", "install", "numpy==1.19.5", "tensorforce==0.6.5", "gym==0.26.0", "flask==2.2.2"])
+
+#VERSIONS
+#Python Version: 3.10.6
+#Ubuntu Version: 22.04.1
+#IDE: Visual Studio Code
+
+#Note that you should run this script in a command prompt or terminal window, 
+#and ensure that the Python executable is added to your system PATH. 
+#Additionally, you should ensure that you have an active internet connection 
+#so that the required libraries can be downloaded from PyPI.
